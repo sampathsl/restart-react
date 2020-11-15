9ee048fda8d88bcec238b1a40d473b6510c611ab
Sampath Thennakoon <sampath.thennakoon@gmail.com>
2020-11-15 09:57:18 +0530

    Add react compoments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <>
+
+        <div className="App">
+          <div className="App-header">
+            <MyActualHeader/>
+            <MyHead/>
+          </div>
+        </div>
+
+      </>
+  );
+}
+
+function MyActualHeader() {
+  return (
+      <>
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </>
   );
 }
+
+function MyHead() {
+  return <div id="test"> My Amazing Header {new Date().toLocaleString()} </div>
+}
+
+setInterval(function () {
+  ReactDOM.render(<MyHead/>, document.getElementById('test'));
+}, 1000);
 
 export default App;
